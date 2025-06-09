aedf178a5e1b5a421981b8f0f4bec23579f5d5b5
Sayed Sheikh <sayedsheikh9@gmail.com>
2025-06-10 05:07:46 +0600

    footer modified

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
       <aside>
         <p>
-          Copyright © {new Date().getFullYear()} - All right reserved by ACME
-          Industries Ltd
+          Copyright © {new Date().getFullYear()} - All right reserved by DIU CPC
+          2025
         </p>
       </aside>
     </footer>
